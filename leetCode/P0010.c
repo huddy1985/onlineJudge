f8ab814a6a3adc5d0dfcb63ec39ce41b19fb688b
汪寒成 <171094685@qq.com>
2015-07-19 20:32:25 +0800

    Regular Expression Matching.

--- leetCode/P0010.c
+++ leetCode/P0010.c
@@ -1,40 +1,52 @@
-
-
+#include <stdio.h>
+#include <string.h>
 bool isMatch(char* s, char* p) {
 	if(NULL == s && NULL == p)
 		return true;
 	if(NULL == s && NULL != p)
 		return false;
-	if(NULL != s && NULL == P)
+	if(NULL != s && NULL == p)
 		return false;
+	int flag[2] = {-1.-1};
 	int len_s = strlen(s);
 	int len_p = strlen(p);
-	if(len_p == 0 && len_s == 0)
-		return true;
-	
-	int cnt_s = 0,cnt_p = 0;
-	int flag = 0;
-	while(cnt_p < len_p){
-		if(p[cnt_p++] == '*' || flag == 1){
-			while(p[cnt_p] != '*' && p[cnt_p] != '.'){
-				cnt_p ++;
+	int p_i = 0,s_i = 0;
+	while(p_i < len_p && s_i < len_s){
+		if(p[p_i] == '.'){
+			s_i ++;p_i ++;
+		}else if(p[p_i] == '*'){
+			flag[1] = p_i+1;
+			p_i ++;
+			while(p_i < len_p && s_i < len_s){
+				if(s[s_i++] == p[p_i]){
+					p_i ++;
+					flag[0] = s_i;
+					break;
+				}
 			}
-			while(s[cnt_s] != p[cnt_p]){
-				cnt_s ++;
-			}
-			if(p[cnt_p] == '*'){
-				flag = 1;
-			}
-			
-			
-		}
-		else if(p[cnt_p] == '.'){
-			cnt_p ++;
-			cnt_s ++;
-		}
-		else{
-			if(s[cnt_s++] != p[cnt_p]++)
+			if(s_i == len_s)
 				return false;
-		}
+			else if(p_i == len_p)
+				return true;
+		}else if(p[p_i] == s[s_i]){
+			p_i ++; s_i ++;
+		}else if(flag[0] >= 0){
+			s_i = flag[0];p_i = flag[1];
+			while(p_i < len_p && s_i < len_s){
+				if(s[s_i++] == p[p_i]){
+					p_i ++;
+					flag[0] = s_i;
+					break;
+				}
+			}
+			
+			if(s_i == len_s)
+				return false;
+		}else
+			return false;
 	}
+	if(s_i == len_s && p_i == len_p)
+		return true;
+	else
+		return false;
 }
